634dd2fd29e4ebde9807d484ad2ea48b1d73b786
srujana <gurrala.srujana@outlook.com>
2022-10-18 09:30:03 +0200

    com15

--- src/app.js
+++ src/app.js
@@ -17,12 +17,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
-  });
-}
 // Use movie router
 app.use("/api/v1/songs", songRouter);
 
